c3a564285ddcdc3c5af6af2ad9fff39d9596b102
Abdulrahmanelheyb <abdulrahmanelheyb@outlook.com>
2020-10-29 22:34:09 +0300

    Add project files.

---
+++ StringReverser/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringReverser
+{
+    class Program
+    {
+        private static void Dialog()
+        {
+            Console.Title = "String Reverser ABCD | DCBA";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Please enter Text to reverse ");
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine("With Array Reverse :");
+            string rlta = Reverse.executeWithArrayReverse((Console.ReadLine()));
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Reversed String = " + rlta);
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("------------------------------------------------------");
+            Console.WriteLine("Do you want try again ? ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("For Yes = y For No = Press any Key ");
+            Console.ResetColor();
+            if (Console.ReadKey().Key == ConsoleKey.Y)
+            {
+                Console.Clear();
+                Dialog();
+            }
+            else
+            {
+                Environment.Exit(0);
+            }
+            
+
+        }
+        static void Main(string[] args)
+        {
+            Dialog();
+        }
+    }
+}
---
+++ StringReverser/Reverse.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace StringReverser
+{
+    public class Reverse
+    {
+        public static string executeWithArrayReverse(string text)
+        {
+            Stopwatch ws = new Stopwatch();
+            ws.Start();
+            var words = text.Split(' ');
+            string rlt = null;
+            for (int i = 0; i < words.Length; i++)
+            {
+                char[] chrs = words[i].ToCharArray();
+                for (int j = (chrs.Length - 1); j >= 0; j--)
+                {
+                    rlt += chrs[j];
+                }
+                rlt += " ";
+            }
+
+            ws.Stop();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Watch Time is : " + ws.Elapsed.ToString());
+            Console.ResetColor();
+            return rlt;
+        }
+    }
+}
